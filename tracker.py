18b010a3b7e030ecedb09d79c0be0a71341f1127
Jos Elkink <jos@cantriii.org>
2021-12-03 12:29:23 +0000

    More efficient storage of data in tracker class.

--- tracker.py
+++ tracker.py
@@ -6,29 +6,31 @@ class Tracker:
     self.simulation = simulation
     self.data = []
 
-  ## Note that this is a very memory inefficient way to store
-  ## the data, as each time step, all party names and strategies
-  ## are saved, and for each data point, the names of the variables
-  ## ("time", "party", etc.) are saved to memory again. Much more
-  ## efficient to just store the numerical data and write a more
-  ## complicated save_output_file that figures out what is what.
   def save_state(self):
     for party in self.simulation.parties:
-      self.data.append({
-        "time": self.simulation.time,
-        "party": party.name,
-        "strategy": party.strategy,
-        "x": party.location.x,
-        "y": party.location.y,
-        "votes": party.votes()
-      })
+      self.data.append([
+        self.simulation.time,
+        party.location.x,
+        party.location.y,
+        party.votes()
+      ])
     
   def save_output_file(self):
+
+    party_names = []
+    party_strategies = []
+    for party in self.simulation.parties:
+      party_names.append(party.name)
+      party_strategies.append(party.strategy)
+    n_parties = len(self.simulation.parties)
+
     with open(self.simulation.config.get_output_filename(), "w") as output:
       output_writer = csv.writer(output)
       output_writer.writerow(["time", "party", "strategy", "x", "y", "votes"])
       
-      for line in self.data:
-        output_writer.writerow(line.values())
+      for i, line in enumerate(self.data):
+        output_writer.writerow([
+          party_names[i % n_parties],
+          party_strategies[i % n_parties]] + line)
 
     self.simulation.log.write("Output saved to \"" + self.simulation.config.get_output_filename() + "\"")
